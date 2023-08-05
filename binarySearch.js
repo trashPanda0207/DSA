178f569ecaa8291568a0b599a8c6ac08335e5849
trashPandaFromUbuntu <arrogant10190@gmail.com>
2023-08-05 22:07:48 +0800

    Edited.

--- binarySearch.js
+++ binarySearch.js
@@ -1,13 +1,11 @@
 function binarySearch(arr, num) {
   min = 0;
   max = arr.length - 1;
-  step = 0;
 
   while (min <= max) {
     let mid = Math.floor((max + min) / 2);
     if (num > arr[mid]) {
       min = mid + 1;
-      console.log("Now min's index: " + min + ", value: " + arr[min]);
     } else if (num < arr[mid]) {
       max = mid - 1;
     } else if (num === arr[mid]) {
