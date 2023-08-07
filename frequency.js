4de670a76920eeb5929c2f81d1877251946063bc
trashPandaFromAsia <arrogant10190@gmail.com>
2023-08-07 10:07:07 +0800

    Completed frequency.js

--- frequency.js
+++ frequency.js
@@ -1,3 +1,36 @@
-function frequency(str1, str2) {}
+function frequency(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let counter1 = {};
+  let counter2 = {};
+
+  for (let i in str1) {
+    if (!counter1[str1[i]]) {
+      counter1[str1[i]] = 1;
+    } else {
+      counter1[str1[i]]++;
+    }
+  }
+
+  for (let j in str2) {
+    if (!counter2[str2[j]]) {
+      counter2[str2[j]] = 1;
+    } else {
+      counter2[str2[j]]++;
+    }
+  }
+
+  for (let property in counter1) {
+    if (!counter2[property]) {
+      return false;
+    }
+    if (counter2[property] !== counter1[property]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 console.log(frequency("acbb", "abbc"));
