bffc437fd2e28b1ad055e6ce6157c69693990a98
trashPandaFromAsia <arrogant10190@gmail.com>
2023-08-08 11:04:58 +0800

    Completed averagePair.js

--- averagePair.js
+++ averagePair.js
@@ -1,14 +1,24 @@
 function ap(arr, n) {
-  let result = [];
+  let sample = new Set();
+  let result = new Set();
   let doubleN = n * 2;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr.includes(doubleN - arr[i])) {
-      result.push([arr[i], doubleN - arr[i]]);
-    }
-  }
+  arr.forEach((i) => {
+    sample.add(i);
+  });
 
-  console.log(result);
+  sample.forEach((x) => {
+    let y = doubleN - x;
+    let sortedPair = [x, y].toString();
+    let reversePair = [y, x].toString();
+    if (sample.has(y) && !result.has(sortedPair) && !result.has(reversePair)) {
+      result.add(sortedPair);
+    }
+  });
+
+  result.forEach((element) => {
+    console.log(element.split(",").map(Number));
+  });
 }
 
 ap([-11, 0, 1, 2, 3, 9, 14, 17, 21], 1.5);
